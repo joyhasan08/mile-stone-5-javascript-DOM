32d9419b9955298f816f4169ee2f537e53217da9
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2023-08-12 16:00:48 +0600

    html added

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log(document);
+console.log(document); 
 console.log(document.body);
+let listItems = document.getElementsByTagName('li');
+console.log(listItems);
+let pTags = document.getElementsByTagName('p');
+
+let titleText = document.getElementsByClassName('title');
+console.log(titleText);
+
+let mainHeader = document.getElementById('main-header')
+console.log(mainHeader);
+mainHeader.style.color='lightblue'
